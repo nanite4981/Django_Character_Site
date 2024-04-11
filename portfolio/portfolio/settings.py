d6f19a4e4b7ed695f0a4f353d041340e7d7d5e95
shaunvann <shaunvann36@gmail.com>
2024-04-11 03:26:40 -0600

    Forgot to set debug false

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#126o=f6!1%p-yu40mvk7it%k*=nsz(9)+yfnbptgt$*qm-(-t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
